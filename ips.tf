5088fcf8d47dc5f9c14151fa1aa6f806437dc544
Miki <108610162+MK2I@users.noreply.github.com>
2025-03-31 14:04:03 +0200

    Update ips.tf
    cleared

--- ips.tf
+++ ips.tf
@@ -2,6 +2,6 @@ resource "azurerm_public_ip" "pip" {
  name = "dbip"
  location = "West Europe"
  resource_group_name = "${azurerm_resource_group.rg.name}"
- domain_name_label = "dbip" 
+ domain_name_label = "dbip"
  allocation_method = "Dynamic"
 }
